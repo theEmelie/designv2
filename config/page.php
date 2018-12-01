a7642baa1cb1e160e517724b2e919d37d2da1edc
Emelie Åslund <theem@live.se>
2018-12-01 18:11:22 +0000

    finished with kmom04

--- config/page.php
+++ config/page.php
@@ -20,7 +20,7 @@ return [
             "stylesheets" => [
                 // Change here to set another default stylesheet
                 //"css/dbwebb-se.min.css",
-                "css/04minimalistic.min.css",
+                "css/04_minimalistic.min.css",
             ],
             "javascripts" => [
                 "js/responsive-menu.js",
